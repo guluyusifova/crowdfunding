55eb534143ed921ddf3bb14c443f5a1574acae6a
Gulu <gl.yusifova@gmail.com>
2024-05-08 23:27:07 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="title">CrowdFunding</div>
+  <div class="totalFunding">Total Funding: ${{ totalRaised }}</div>
+  <div class="mainpart">
+    <box
+      @myEvent="totalFunding"
+      v-for="bx in boxdata"
+      :projectnum="bx.projectnum"
+      :projectgoal="bx.projectgoal"
+      :amountraised="bx.amountraised"
+      :remainingamount="bx.remainingamount"
+    />
+  </div>
+</template>
+
+<script>
+import box from "./components/box.vue";
+export default {
+  components: {
+    box,
+  },
+  methods: {
+    totalFunding(newDonation) {
+      this.totalRaised += newDonation;
+    },
+  },
+  data() {
+    return {
+      totalRaised: 0,
+      boxdata: [
+        {
+          projectnum: "1",
+          projectgoal: 1000,
+        },
+        {
+          projectnum: "2",
+          projectgoal: 500,
+        },
+        {
+          projectnum: "3",
+          projectgoal: 2000,
+        },
+        {
+          projectnum: "4",
+          projectgoal: 1500,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+.title {
+  color: blue;
+  background-color: beige;
+  border: 2px solid red;
+  border-radius: 5px;
+  text-align: center;
+  font-size: 30px;
+  margin: 0 110px;
+}
+
+.totalFunding {
+  color: black;
+  background-color: beige;
+  border: 1px solid black;
+  border-radius: 5px;
+  text-align: center;
+  font-size: 20px;
+  margin: 10px 500px;
+}
+
+.mainpart {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/box.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="box">
+        <h1> Project  {{ projectnum}}</h1>
+        <div> Project goal: ${{projectgoal}} </div>
+        <br>
+        <div> Amount raised: ${{amountraised}}</div>
+        <br>
+        <input type="number" placeholder="Enter Amount Here"  v-model="inputValue">  </input>
+        <Button @click="calculateAmount"> Donate </Button>
+        <br>
+        <br>
+        <div> Remaining amount needed: ${{remainingamount}}</div>
+        <br>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["projectnum", "projectgoal"],
+    data(){
+     return {
+        amountraised:0,
+        remainingamount: this.projectgoal,
+     }
+    },
+    methods: {
+        calculateAmount() {
+            this.$emit('myEvent', this.inputValue);
+            this.amountraised += this.inputValue
+            this.remainingamount = this.projectgoal - this.amountraised
+            this.inputValue= ""
+        }
+    },
+ }
+</script>
+
+<style scoped lang="scss">
+.box {
+    border: 1px solid blue;
+    border-radius: 5px;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.7);
+    width: 300px;
+    margin: 10px;
+    text-align: center;
+    
+};
+</style>
